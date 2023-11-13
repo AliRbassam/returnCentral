e79a65adeae374117c383a811af6a1db735daa71
aachour <achour1990@gmail.com>
2023-11-13 18:31:34 +0200

    fixes1 home page

--- includes/bottom.php
+++ includes/bottom.php
@@ -2,94 +2,63 @@
 	<!------Footer------>
 	<div class="section greenBg topSpacer"></div>
 
+	<div id="footer" class="blueBg">
 
-	<!-- starting  -->
-	<div id="footer">
-    <div class="content">
+        <div class="content">
+    
+            <div>
+                <img src="../../static/images/logo.png" width="250px" />
+            </div>
 
-        <!-- First Row: Logo -->
-        <footer class="footer-section">
-            <div class="container">
-                <div class="footer-content pt-5 pb-5">
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="footer-logo">
-                                <a href="#"><img src="../../static/images/logo.png" class="img-fluid" alt="logo"></a>
-                            </div>
-                        </div>
+            <div class="topSpacerBig row">
+                
+                <div class="col-lg-1">&nbsp;</div>
+
+                <div class="col-lg-4">
+                    <div class="small white">Namd of state, United States <br> xxxxxxx@returncentralrx.com</div>
+                </div>
+
+                <div class="col-lg-1">&nbsp;</div>
+                
+                <div class="col-lg-2">
+                    <div class="small white">Overview <br>Our Process</div>
+                </div>
+                <div class="col-lg-2">
+                    <div class="small white">FAQ <br>Contact Us</div>
+                </div>
+                <div class="col-lg-2">
+                    <a href="#" target="_blank"><i class="fab fa-instagram"></i>AAA</a>
+                    <a href="#" target="_blank"><i class="fab fa-facebook"></i>AAA</a>
+                    <a href="#" target="_blank"><i class="fab fa-linkedin"></i>AAA</a>
+                </div>
+                
+                
+            </div>
+
+            <div class="topSpacerBig row">
+                
+                <div class="col-lg-1">&nbsp;</div>
+
+                <div class="col-lg-4">
+                    <div class="tiny white">For any further inquiries about RETURN CENTRAL RX Private Label Supplements, our manufacturing process, and a comparative analysis with our competitors, please feel free to reach out to us via email at xxxx@returncentralrx.com.</div>
+                    <div class="topSpacerSmall tiny white">We are available to assist you from [Day] to [Day], between [Time] am and [Time] pm</div>
+                </div>
+
+                <div class="col-lg-1">&nbsp;</div>
+                
+                <div class="col-lg-6">
+                    <div class="topSpacer medium white">Subscribe to our updates</div>
+                    <div class="topSpacerSmaller form-group">
+                        <input type="button" class="form-control" id="email" name="email" placeholder="Email">
                     </div>
                 </div>
+                
             </div>
-        </footer>
 
-        <!-- Second Row: Footer Text, Overview, FAQ, Contact, Social Icons -->
-        <footer class="footer-section">
-            <div class="container">
-                <div class="footer-content pt-5 pb-5">
-                    <div class="row">
-                        <div class="col-xl-4 col-lg-4 mb-50">
-                            <!-- Footer Text -->
-                            <div class="footer-widget">
-                                <div class="footer-text">
-                                    <p>Namd of state, United States <br> xxxxxxx@returncentralrx.com</p>
-                                </div>
-                                <div class="footer-text">
-                                    <p>For any further inquiries about RETURN CENTRAL RX Private
-										Label Supplements, our manufacturing process, and a
-										comparative analysis with our competitors, please feel free to
-										reach out to us via email at xxxx@returncentralrx.com. <br>
-										<BR>We are available to assist you from [Day] to [Day], between
-										[Time] am and [Time] pm</p>
-                                </div>
-                            </div>
-                        </div>
 
-                        <div class="col-xl-4 col-lg-4 col-md-6 mb-30">
-                            <!-- Overview and Social Icons -->
-                            <div class="footer-widget">
-                                <div class="footer-widget-heading">
-                                    <h3>Overview <br>Our Process</h3>
-                                </div>
-                                <div class="footer-social-icon">
-                                    <a href="#" target="_blank"><i class="fab fa-instagram"></i>AAA</a>
-                                    <a href="#" target="_blank"><i class="fab fa-facebook"></i>AAA</a>
-                                    <a href="#" target="_blank"><i class="fab fa-linkedin"></i>AAA</a>
-                                </div>
-                            </div>
-                        </div>
+        </div>
 
-                        <div class="col-xl-4 col-lg-4 col-md-6 mb-50">
-                            <!-- FAQ, Contact, and Subscribe -->
-                            <div class="footer-widget">
-                                <div class="footer-widget-heading">
-                                    <h3>FAQ <br>Contact Us</h3>
-                                </div>
-                                <div class="footer-text mb-25">
-                                    <p>Subscribe to our updates</p>
-                                </div>
-                                <div class="form-group">
-                                    <label for="email">Email</label>
-                                    <input type="button" class="form-control" id="email" name="email" required>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </footer>
-
-        <!-- Third Row: Text, Data, and Subscribe -->
-        <footer class="footer-section">
-            <div class="container">
-                <div class="footer-content pt-5 pb-5">
-                    <div class="row">
-                        <!-- Add your text, data, and subscribe elements here -->
-                    </div>
-                </div>
-            </div>
-        </footer>
     </div>
-</div>
 
 
 </body>
